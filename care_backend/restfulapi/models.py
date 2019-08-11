c976026748b11cbea1cee3675e938e6d5301d89f
Ricardo Serrano <rxsh964@gmail.com>
2019-08-11 17:36:34 -0500

    user endpoint

--- care_backend/restfulapi/models.py
+++ care_backend/restfulapi/models.py
@@ -1,27 +1,7 @@
 from django.db import models
+from restfulapi.manager import UserManager
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
-
-
-class UserManager(BaseUserManager):
-
-	def create_user(self, email, password=None, **extra_fields):
-		'''Create and saves a new user'''
-		if not email:
-			raise ValueError('Users must have an email address')
-		user=self.model(email=self.normalize_email(email), **extra_fields)
-		user.set_password(password)
-		user.save(using=self._db)
-		return user
-
-
-	def create_superuser(self, email, password):
-		'''Creates and saves a super user'''
-		user=self.create_user(email, password)
-		user.is_staff=True
-		user.is_superuser=True
-		user.save(using=self._db)
-		return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -29,6 +9,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 	email=models.EmailField(max_length=255, unique=True)
 	name=models.CharField(max_length=255)
 	lastname=models.CharField(max_length=255)
+	phone_number=models.CharField(max_length=10)
 	is_active=models.BooleanField(default=True)
 	is_staff=models.BooleanField(default=False)
 	objects=UserManager()
