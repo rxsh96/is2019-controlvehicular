aa0d4b9a8f4bbcd3a013835094c1991207122ce7
Steven <stvn.piano@gmail.com>
2019-08-24 13:41:59 -0500

    Add routes file

--- care_app/lib/main.dart
+++ care_app/lib/main.dart
@@ -1,14 +1,8 @@
-import 'package:care_app/src/pages/filtros_page.dart';
-import 'package:care_app/src/pages/agregarVehiculo_page.dart';
-import 'package:care_app/src/pages/auth/login_page.dart';
-import 'package:care_app/src/pages/frenos_page.dart';
-import 'package:care_app/src/pages/gasolina_page.dart';
-import 'package:care_app/src/pages/kms_page.dart';
-import 'package:care_app/src/pages/luces_page.dart';
-import 'package:care_app/src/pages/multas_page.dart';
-import 'package:care_app/src/pages/nuevoUser_page.dart';
-import 'package:care_app/src/pages/vehiculos_page.dart';
+
 import 'package:flutter/material.dart';
+
+import 'package:care_app/src/pages/auth/login_page.dart';
+import 'package:care_app/src/routes/routes.dart';
 
 
 void main() {
@@ -17,20 +11,7 @@ void main() {
        home: MyApp(),
        debugShowCheckedModeBanner: false,
        initialRoute: '/',
-      routes: {
-        '/login'           : ( context ) => MyLoginPage(),
-        '/nuevoUser'       : ( context ) => NuevoUserPage(),
-        '/vehiculos'       : ( context ) => VehiculosPage(),
-        '/agregarVehiculos': ( context ) => AgregarVehiculoPage(),
-        '/filtros'         : ( context ) => FiltroPage(),
-        '/frenos'          : ( context ) => FrenosPage(),
-        '/luces'           : ( context ) => LucesPage(),
-        '/multas'          : ( context ) => MultasPage(),
-        '/kilometraje'     : ( context ) => KilometrajePage(),
-        '/gasolina'        : ( context ) => GasolinaPage()
-        
-       },
-
+      routes: getRoutes(),
 
       //Tema general para el appbar
       theme: ThemeData(
---
+++ care_app/lib/src/routes/routes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:care_app/src/pages/agregarVehiculo_page.dart';
+import 'package:care_app/src/pages/auth/login_page.dart';
+import 'package:care_app/src/pages/filtros_page.dart';
+import 'package:care_app/src/pages/frenos_page.dart';
+import 'package:care_app/src/pages/gasolina_page.dart';
+import 'package:care_app/src/pages/kms_page.dart';
+import 'package:care_app/src/pages/luces_page.dart';
+import 'package:care_app/src/pages/multas_page.dart';
+import 'package:care_app/src/pages/nuevoUser_page.dart';
+import 'package:care_app/src/pages/vehiculos_page.dart';
+
+
+
+Map <String , WidgetBuilder > getRoutes(){
+
+  return <String, WidgetBuilder>{
+        '/login'           : ( context ) => MyLoginPage(),
+        '/nuevoUser'       : ( context ) => NuevoUserPage(),
+        '/vehiculos'       : ( context ) => VehiculosPage(),
+        '/agregarVehiculos': ( context ) => AgregarVehiculoPage(),
+        '/filtros'         : ( context ) => FiltroPage(),
+        '/frenos'          : ( context ) => FrenosPage(),
+        '/luces'           : ( context ) => LucesPage(),
+        '/multas'          : ( context ) => MultasPage(),
+        '/kilometraje'     : ( context ) => KilometrajePage(),
+        '/gasolina'        : ( context ) => GasolinaPage()
+      
+  };
+}
