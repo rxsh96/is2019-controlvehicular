0676f37ffac0178d063c8007122850da0c9e161f
alexgor10 <32745067+alexgor10@users.noreply.github.com>
2019-09-08 21:29:12 -0500

    CRUD usuario

---
+++ care_backend/user/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+from api.models import User
+# Create your models here.
---
+++ care_backend/user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from user.views import UserListView,UserDetailView,UserUpdateView,UserDeleteView
+
+urlpatterns = [
+  path('user/', UserListView.as_view(), name="usuario"),
+  path('user/<int:pk>',UserDetailView.as_view(), name="detalle_usuario"),
+  path('user/update/<int:pk>',UserUpdateView.as_view(), name="actualizar_usuario"),
+  path('user/delete/<int:pk>',UserDeleteView.as_view(), name="eliminar_usuario"),
+]
