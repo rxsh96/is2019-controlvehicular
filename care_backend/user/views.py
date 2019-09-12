5df269b20eccf0291d4fa6c91125fbfd56ad71bb
AnMena <fullmena_andremist@hotmail.com>
2019-09-12 15:55:11 -0500

    Web backend: create user +

--- care_backend/user/views.py
+++ care_backend/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from user.models import User
-from user.forms import UserUpdateForm
+from user.forms import UserUpdateForm, UserCreateForm
 from django.urls import reverse_lazy
 from bootstrap_modal_forms.generic import (BSModalCreateView,
                                            BSModalUpdateView,
