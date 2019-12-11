6774be996d36c559dc19e20c444d2c12767dbf89
AnMena <fullmena_andremist@hotmail.com>
2019-12-10 20:54:47 -0500

    DB Script marca-modelo

--- care_backend/marca-modelo.py
+++ care_backend/marca-modelo.py
@@ -1,12 +1,12 @@
 import psycopg2
 conn = psycopg2.connect("host=localhost dbname=care_db user=postgres password=postgres")
 cur = conn.cursor()
-with open('csv-provincias.csv', 'r') as f:
+with open('marca.csv', 'r') as f:
     next(f) # Skip the header row.
-    cur.copy_from(f, 'api_province', sep=',')
+    cur.copy_from(f, 'api_brand', sep=',')
     conn.commit()
-with open('csv-ciudades.csv', 'r') as f:
+with open('modelo.csv', 'r') as f:
     next(f) # Skip the header row.
-    cur.copy_from(f, 'api_city', sep=',')
+    cur.copy_from(f, 'api_model', sep=',')
     conn.commit()
 conn.close()
