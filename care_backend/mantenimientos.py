fac80e6100d9e1cef261d7c73183220f3d5d91a7
Ricardo Serrano <rxsh964@gmail.com>
2020-01-09 11:21:33 -0500

    [#169218604] comments and suggestions

--- care_backend/mantenimientos.py
+++ care_backend/mantenimientos.py
@@ -1,7 +1,7 @@
 import psycopg2
 conn = psycopg2.connect("host=localhost dbname=care_db user=postgres password=postgres")
 cur = conn.cursor()
-with open('csv-maintenance.csv', 'r') as f:
+with open('csv-maintenance.csv', 'r', encoding='utf-8') as f:
     next(f) # Skip the header row.
     cur.copy_from(f, 'api_maintenance', sep=',')
     conn.commit()
--- care_backend/provincia-ciudad.py
+++ care_backend/provincia-ciudad.py
@@ -1,11 +1,12 @@
 import psycopg2
+
 conn = psycopg2.connect("host=localhost dbname=care_db user=postgres password=postgres")
 cur = conn.cursor()
-with open('csv-provincias.csv', 'r') as f:
+with open('csv-provincias.csv', 'r', encoding='utf-8') as f:
     next(f) # Skip the header row.
     cur.copy_from(f, 'api_province', sep=',')
     conn.commit()
-with open('csv-ciudades.csv', 'r') as f:
+with open('csv-ciudades.csv', 'r', encoding='utf-8') as f:
     next(f) # Skip the header row.
     cur.copy_from(f, 'api_city', sep=',')
     conn.commit()
